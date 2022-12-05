08de1c41e1ee1e01ea286352face654e3b84f72e
Mark Marusic <78791564+MarkMaru@users.noreply.github.com>
2022-12-04 20:04:53 -0500

    Creation
    New project started

---
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    @Published var currentLocation: CLLocation?
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last, currentLocation == nil else { return }
+        
+        DispatchQueue.main.async {
+            self.currentLocation = location
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by GCCISAdmin on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
