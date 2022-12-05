1d280f46f331b1e6b0312932970ac3ed6bf44834
Mark Marusic <78791564+MarkMaru@users.noreply.github.com>
2022-12-04 20:29:41 -0500

    Add files via upload

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import CoreLocation
+import WeatherKit
 
 class LocationManager: NSObject, ObservableObject {
     @Published var currentLocation: CLLocation?
@@ -26,14 +27,27 @@ extension LocationManager: CLLocationManagerDelegate {
 }
 
 struct ContentView: View {
+    
+    let weatherService = WeatherService.shared
+    
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+        }.task(id: locationManager.currentLocation) {
+            do {
+                if let location = locationManager.currentLocation {
+                    let weather = try await weatherService.weather(for: location)
+                    print(weather)
+                }
+            } catch {
+                print(error)
+            }
         }
-        .padding()
     }
 }
 
